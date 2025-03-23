471f1782ae12449a9a0f78aba520d49a34f59631
saajan376 <saajan.varghese.2006@gmail.com>
2025-03-23 19:41:31 +0530

    update

---
+++ Inheritance/inheritance-3.py
@@ -0,0 +1,9 @@
+class person(object):
+    def __init__(self,name,id):
+        self.name=name
+        self.id=id
+    def display(self):
+        print(self.name, self.id)
+
+emp=person("Satyam",102)
+print(emp.display())
