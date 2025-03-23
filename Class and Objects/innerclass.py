3e40d1f41ef02df0b2cb60531e69c55340a7c817
saajan376 <saajan.varghese.2006@gmail.com>
2025-03-23 15:04:18 +0530

    update

---
+++ Class and Objects/innerclass.py
@@ -0,0 +1,22 @@
+class student:
+    def __init__(self, name, rollno):
+        self.name=name
+        self.rollno=rollno
+        self.lap=self.laptop()
+
+    def show(self):
+        print(self.name, self.rollno)
+        self.lap.show()
+
+    class laptop:
+        def __init__(self):
+            self.brand="hp"
+            self.cpu="i5"
+            self.ram=8
+        
+        def show(self):
+            print(self.brand, self.cpu, self.ram)
+
+s1=student('navin',2)
+s2=student('jenny',3)
+s1.show()
